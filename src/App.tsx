5a5dd8186290d576c7a7667ee8d9f56bb814b54c
yashpal singh <yashpalsingh9rx@outlook.com>
2024-04-06 14:38:11 +0530

    login done

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,36 @@
 import './App.css'
+import {useState} from "react";
 // import {useState} from "react";
 // import {useState} from "react";
 
 function App() {
     console.log("Mount");
-    // const [canStart,setCanStart] = useState<boolean>(true);
+    const [canStart,setCanStart] = useState<boolean>(true);
+    const [message,setMessage] = useState<string>('')
     // const [canStop,setCanStop] = useState<boolean>(false);
     // const [chunk,setChunk]= useState<Blob>();
-    // const [recorder,setRecorder]=useState<MediaRecorder|null>(null)
+    const [recorder,setRecorder]=useState<MediaRecorder|null>(null)
     // const [stream,setStream]=useState<MediaStream|null>(null)
     // const [audio,setAudio]=useState<MediaStream|null>(null)
     // const [mixedStream,setmixedStream] = useState()
     // let audio:MediaStream|null = null,
     //     mixedStream = null
 
-    let startButton: boolean = true;
-    let stopButton: boolean = false;
-    let chunk: Blob[] = [];
-    let recorder: MediaRecorder | null = null;
     let stream: MediaStream | null = null;
-    let audio: MediaStream | null = null;
-    let mixedStream;
+    let chunk: Blob[] = [];
+    // let recorder: MediaRecorder;
 
 
     async function setupStream() {
         try {
             stream = await navigator.mediaDevices.getDisplayMedia({
                 video: true,
-            });
-            audio = await navigator.mediaDevices.getUserMedia({
                 audio: {
                     echoCancellation: true,
                     noiseSuppression: true,
                     sampleRate: 44100
                 }
-            })
+            });
             await setUpVideoFeedback();
         } catch (err) {
             console.log(err)
@@ -58,7 +54,10 @@ function App() {
     }
 
     function handleStop() {
-        const blob = new Blob(chunk, {'type': 'video/mp4'});
+        console.log('yes');
+        setCanStart(true)
+        setMessage('');
+        const blob = new Blob(chunk, {type: 'video/mp4'});
         chunk = [];
         const downloadButton = document.querySelector('.download-video') as HTMLAnchorElement
         if (downloadButton) {
@@ -69,27 +68,28 @@ function App() {
         recordedVideo.src = URL.createObjectURL(blob)
         recordedVideo.load()
         recordedVideo.onloadeddata = async function () {
+            const rc = document.querySelector(".recorded-video-wrap") as HTMLDivElement
+            rc.classList.remove("hidden");
+            rc.scrollIntoView({ behavior: "smooth", block: "start" });
             await recordedVideo.play()
         }
-
-        if(stream && audio){
-            stream.getTracks().forEach((track) => track.stop());
-            audio.getTracks().forEach((track) => track.stop());
-        }
+        stream && stream.getTracks().forEach((track) => track.stop());
+        console.log('stopped');
     }
 
     async function startRecording() {
         await setupStream();
 
-        if (stream && audio) {
-            mixedStream = new MediaStream([...stream.getTracks(), ...audio.getTracks()]);
-            recorder = new MediaRecorder(mixedStream);
-            recorder.ondataavailable = handleDataAvailable;
-            recorder.onstop = handleStop;
-            recorder.start(1000);
+        if (stream) {
+            const record = new MediaRecorder(new MediaStream([...stream.getTracks()]));
+            setRecorder(record);
+            record.ondataavailable = handleDataAvailable;
+            record.onstop = handleStop;
+            record.start(1000);
+            // recorder.ondataavailable = handleDataAvailable
 
-            startButton = false;
-            stopButton = true;
+            setCanStart(false);
+            setMessage('Recording... 📷')
             console.log('Recording started');
         } else {
             console.log('No stream Available')
@@ -97,23 +97,26 @@ function App() {
     }
 
     function stopRecording() {
+        console.log('from here')
         if (recorder) {
             recorder.stop();
-            startButton = true;
-            stopButton = false;
             console.log('recording stopped');
         } else {
             console.log('recorder is null even after starting recording');
         }
     }
 
+    const startButt:string = !canStart ? 'border-red-800':''
+    const stopButt:string = canStart ? 'border-red-800':''
+
     return (
         <section>
-            <button onClick={startRecording} className={'border-2 border-amber-500'}>start recording</button>
-            <button onClick={stopRecording} className={'border-2 border-amber-500'}>stop recording</button>
+            <button onClick={startRecording} disabled={!canStart} className={`border-2 border-amber-500 ${startButt}`}>start recording</button>
+            <button onClick={stopRecording} disabled={canStart} className={`border-2 border-amber-500 ${stopButt}`}>stop recording</button>
+            <p>{message}</p>
             <video src="" className="video-feedback bg-black w-96 h-64 mb-4" autoPlay></video>
 
-            <div className="recorded-video-wrap">
+            <div className="recorded-video-wrap hidden">
                 <h2 className="text-xl text-gray-500 uppercase font-light mb-4">
                     Recorded video
                 </h2>
